56ed42919676c4db9833e070390d25f934a0f88a
Rowan <48519289+scar055@users.noreply.github.com>
2026-03-19 14:54:37 +0100

    add get voor nieuws page #5

--- server.js
+++ server.js
@@ -1,39 +1,51 @@
 // Importeer het npm package Express (uit de door npm aangemaakte node_modules map)
 // Deze package is geïnstalleerd via `npm install`, en staat als 'dependency' in package.json
-import express from 'express'
+import express from "express";
 
 // Importeer de Liquid package (ook als dependency via npm geïnstalleerd)
-import { Liquid } from 'liquidjs';
+import {Liquid} from "liquidjs";
 
 // Maak een nieuwe Express applicatie aan, waarin we de server configureren
-const app = express()
+const app = express();
 
 // Maak werken met data uit formulieren iets prettiger
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({extended: true}));
 
 // Gebruik de map 'public' voor statische bestanden (resources zoals CSS, JavaScript, afbeeldingen en fonts)
 // Bestanden in deze map kunnen dus door de browser gebruikt worden
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 // Stel Liquid in als 'view engine'
-const engine = new Liquid()
-app.engine('liquid', engine.express())
+const engine = new Liquid();
+app.engine("liquid", engine.express());
 
 // Stel de map met Liquid templates in
 // Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
-app.set('views', './views')
+app.set("views", "./views");
 
+app.get("/", async function (request, response) {
+  // Render index.liquid uit de Views map
 
-console.log('Let op: Er zijn nog geen routes. Voeg hier dus eerst jouw GET en POST routes toe.')
+  response.render("index.liquid", {path: request.path});
+});
 
-/*
-// Zie https://expressjs.com/en/5x/api.html#app.get.method over app.get()
-app.get(…, async function (request, response) {
-  
-  // Zie https://expressjs.com/en/5x/api.html#res.render over response.render()
-  response.render(…)
-})
-*/
+app.get("/nieuws", async function (request, response) {
+  const params = {
+    "groupBy[]": "title,description,date",
+  };
+  const newsResponse = await fetch(
+    "https://fdnd-agency.directus.app/items/adconnect_news/?" +
+      new URLSearchParams(params),
+  );
+
+  response.render("news.liquid", {path: request.path});
+});
+
+app.get("/lado", async function (request, response) {
+  // Render index.liquid uit de Views map
+
+  response.render("lado.liquid", {path: request.path});
+});
 
 /*
 // Zie https://expressjs.com/en/5x/api.html#app.post.method over app.post()
@@ -69,13 +81,14 @@ app.post(…, async function (request, response) {
 })
 */
 
-
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
 // Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
-app.listen(app.get('port'), function () {
+app.listen(app.get("port"), function () {
   // Toon een bericht in de console
-  console.log(`Daarna kun je via http://localhost:${app.get('port')}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`)
-})
+  console.log(
+    `Daarna kun je via http://localhost:${app.get("port")}/ jouw interactieve website bekijken.\n\nThe Web is for Everyone. Maak mooie dingen 🙂`,
+  );
+});
